783f6b8578bfe3bb735e71b107c9654c4e888718
koji ohno <countryfoxmac@oonoisaoni-no-MacBook-Pro.local>
2016-04-02 11:00:32 +0900

    Unity5対応NinjaSlasherX

---
+++ Assets/Scripts/Menu/Menu_Ending.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Menu_Ending : MonoBehaviour {
+	void Start () {
+		Invoke ("WhiteOut",  2.0f);
+		Invoke ("End"	  , 15.0f);
+	}
+
+	void WhiteOut() {
+		zFoxFadeFilter.instance.FadeOut (Color.white, 12.0f);
+	}
+
+	void End() {
+		PlayerController playerCtrl = PlayerController.GetController();
+		playerCtrl.GameOver ();
+		SceneManager.LoadScene("Menu_HiScore");
+	}
+}
---
+++ Assets/Scripts/StageTrigger_CheckPoint.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class StageTrigger_CheckPoint : MonoBehaviour {
+
+	public string				labelName 	= "";
+	public bool					save 		= true;
+
+	public CameraFollow.Param 	cameraParam;
+
+	void OnTriggerEnter2D_PlayerEvent (GameObject go) {
+		PlayerController.checkPointEnabled   = true;
+		PlayerController.checkPointLabelName = labelName;
+		PlayerController.checkPointSceneName = SceneManager.GetActiveScene().name;
+		PlayerController.checkPointHp 		 = PlayerController.nowHp;
+		Camera.main.GetComponent<CameraFollow>().SetCamera(cameraParam);
+		if (save) {
+			//AppSound.instance.SE_CHECKPOINT.Play();
+			SaveData.SaveGamePlay ();
+		}
+	}
+}
